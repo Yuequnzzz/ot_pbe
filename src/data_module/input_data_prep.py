60183addb804e6a0b8d5021df914ad34d14054bb
Yuequnzzz <yueqzhang@student.ethz.ch>
2024-07-13 11:42:31 +0200

    considered non-pdf bin values, added quantiles for further use

--- src/data_module/input_data_prep.py
+++ src/data_module/input_data_prep.py
@@ -20,23 +20,25 @@ def transform_pdf_to_cdf(prob_df: pd.DataFrame) -> pd.DataFrame:
 
 
 def get_bin_ranges(
-        prob_df: pd.DataFrame,
+        quantile_df: pd.DataFrame,
         bin_ticks: np.ndarray,
-        min_prob: float = 0.0,
+        lower_bound: float,
+        upper_bound: float
 ) -> pd.DataFrame:
     """ Get the ranges of bin values of which the probabilities are
     greater than the minimum probability. This function is useful for
     the case where the bin values are different for different distributions.
 
     Args:
-        prob_df (pd.DataFrame): probabilities in pdf
+        quantile_df (pd.DataFrame): probabilities in pdf
         bin_ticks (np.ndarray): bin values as the x-tick in pdf
-        min_prob (float): the lower bound of probability to use
+        lower_bound (float): the lower bound of quantile to use
+        upper_bound (float): the upper bound of quantile to use
 
     Returns:
         bin_range (pd.DataFrame): upper and lower bounds of bin values for different distributions
     """
-    prob_to_use = np.where(np.array(prob_df) > min_prob)
+    prob_to_use = np.where((np.array(quantile_df) < upper_bound) & (np.array(quantile_df) > lower_bound))
 
     bin_range_df = pd.DataFrame(columns=['min_bin', 'max_bin'])
     for row in np.unique(prob_to_use[0]):
@@ -139,20 +141,30 @@ def get_target_bins(
 
 def get_prob_df(
         df: pd.DataFrame,
-        prefix: str
-) -> pd.DataFrame:
+        prefix: str,
+        dL: float,
+) -> tuple:
     """Get the probability density function from the dataframe
 
     Args:
         df (pd.DataFrame): input dataframe
         prefix (str): the prefix of columns to be selected
+        dL (float): the interval length
 
     Returns:
         prob_df (pd.DataFrame): dataframe with selected columns
+        num_crystal_array (np.array): the new number of crystals
     """
     prob_df = df.filter(like=prefix)
+    # determine if the sum of probabilities for each row is 1
+    if not np.allclose(prob_df.sum(axis=1), 1):
+        # it is not a probability density function
+        num_crystal_array = prob_df.sum(axis=1).values * dL
+        prob_df = prob_df.div(num_crystal_array, axis=0)
+    else:
+        num_crystal_array = None
 
-    return prob_df
+    return prob_df, num_crystal_array
 
 
 def select_columns(
@@ -174,7 +186,9 @@ def prep_feature_table(
         df: pd.DataFrame,
         feature_columns: list,
         num_sample: int,
-        bin_feature: dict
+        bin_feature: dict,
+        quantile_lower_bound: float,
+        quantile_upper_bound: float,
 ) -> tuple:
     """Prepare the input features for certain dataframe containing raw data
 
@@ -183,13 +197,15 @@ def prep_feature_table(
         feature_columns (list): list of column names to be selected
         num_sample (int): the number of inserted points within certain range
         bin_feature (dict): include bin_start and the interval length dL
+        quantile_lower_bound (float): the lower bound of quantile to use
+        quantile_upper_bound (float): the upper bound of quantile to use
 
     Returns:
         input_feature_df (pd.DataFrame): input feature dataframe
         sampled_quantiles_array (np.array): sampled quantile values
     """
     # get the probability dataframe
-    prob_df = get_prob_df(df, 'pop_bin')
+    prob_df, num_crystals = get_prob_df(df, 'pop_bin', bin_feature['dL'])
     num_bin = prob_df.shape[1]
     # get the quantile dataframe
     quantiles_df = transform_pdf_to_cdf(prob_df)
@@ -201,7 +217,12 @@ def prep_feature_table(
     bin_ticks = (bin_start_ticks[1:] + bin_start_ticks[:-1]) / 2
 
     # get the bin ranges
-    bin_range_df = get_bin_ranges(prob_df, bin_ticks)
+    bin_range_df = get_bin_ranges(
+        quantile_df=quantiles_df,
+        bin_ticks=bin_ticks,
+        lower_bound=quantile_lower_bound,
+        upper_bound=quantile_upper_bound
+    )
     # get the sampled bins
     x_bins, source_bins_df = get_sampled_bins(bin_range_df, num_sample)
     # get the sampled quantiles
@@ -213,6 +234,9 @@ def prep_feature_table(
     other_features = select_columns(df, feature_columns).reset_index(drop=True)
     # combine the other features with the sampled bins
     input_feature_df = pd.concat([other_features, source_bins_df], axis=1)
+    # add the number of crystals if it is not None
+    if num_crystals is not None:
+        input_feature_df['ini_mu0'] = num_crystals
 
     return input_feature_df, sampled_quantiles_array
 
@@ -236,7 +260,7 @@ def prep_target_table(
 
     """
     # get the target probability dataframe
-    target_prob_df = get_prob_df(df, 'pop_bin')
+    target_prob_df, num_target_crystals = get_prob_df(df, 'pop_bin', bin_feature['dL'])
     num_bin = target_prob_df.shape[1]
     # get the target quantile dataframe
     target_quantiles_df = transform_pdf_to_cdf(target_prob_df)
@@ -255,6 +279,9 @@ def prep_target_table(
     target_features = select_columns(df, target_columns).reset_index(drop=True)
     # combine the target features with the target bins
     target_feature_df = pd.concat([target_features, target_bins_df], axis=1)
+    # add the number of crystals if it is not None
+    if num_target_crystals is not None:
+        target_feature_df['mu0'] = num_target_crystals
 
     return target_feature_df
 
@@ -267,6 +294,8 @@ def prep_data_for_model(
         target_columns: list,
         bin_feature: dict,
         sample_frac: float,
+        quantile_lower_bound: float,
+        quantile_upper_bound: float
 ) -> tuple:
     """Prepare the feature tables for the model
 
@@ -278,6 +307,8 @@ def prep_data_for_model(
         target_columns (list): list of output column names to be selected
         bin_feature (dict): include bin_start and the interval length dL
         sample_frac (float): the fraction of timepoints in each simulation to sample
+        quantile_lower_bound (float): the lower bound of quantile to use
+        quantile_upper_bound (float): the upper bound of quantile to use
 
     Returns:
         input_df (pd.DataFrame): input feature dataframe
@@ -289,13 +320,14 @@ def prep_data_for_model(
 
     input_df_list = []
     output_df_list = []
+    quantiles_list = []
     for runID in input_mat["runID"]:
         try:
             print("Iteration: ", runID)
             # load single simulation output
             target_sub_df = pd.read_csv(f"{file_path}/PBEsolver_outputs/PBEsolver_{exp_name}_runID{int(runID)}.csv")
             # sample the timepoints to avoid overfitting
-            target_sub_df = target_sub_df.sample(frac=sample_frac)
+            target_sub_df = target_sub_df.sample(frac=sample_frac, random_state=42)
             no_timepoints = target_sub_df.shape[0]
             # get the repeated relevant inputs
             relevant_inputs = input_mat.query("runID == @runID")
@@ -307,7 +339,11 @@ def prep_data_for_model(
                 feature_columns=source_columns,
                 num_sample=num_sample,
                 bin_feature=bin_feature,
+                quantile_lower_bound=quantile_lower_bound,
+                quantile_upper_bound=quantile_upper_bound
             )
+            # append the quantiles
+            quantiles_list.append(sampled_quantiles)
             # add time to the input features
             t_vec = np.array(target_sub_df["t"])[..., np.newaxis]
             input_features_df['t'] = t_vec
@@ -334,6 +370,7 @@ def prep_data_for_model(
 
     input_df = pd.concat(input_df_list, axis=0, ignore_index=True)
     output_df = pd.concat(output_df_list, axis=0, ignore_index=True)
+    quantiles_list = np.vstack(quantiles_list)
 
     if input_df.isnull().values.any():
         raise ValueError(f"There is empty values in input_df")
@@ -348,7 +385,7 @@ def prep_data_for_model(
         'y': np.array(output_df)
     }
 
-    return input_df, output_df, input_datasets
+    return input_df, output_df, input_datasets, quantiles_list
 
 
 if __name__ == '__main__':
@@ -362,6 +399,8 @@ if __name__ == '__main__':
         output_columns,
         bin_start,
         dL,
+        upper_limit,
+        lower_limit,
         sample_frac,
         save_path,
         save_name,
@@ -373,6 +412,8 @@ if __name__ == '__main__':
         data_params["output_columns"],
         data_params["bin_start"],
         data_params["dL"],
+        data_params["upper_bound"],
+        data_params["lower_bound"],
         data_params["sample_fraction"],
         data_params["save_path"],
         data_params["save_name"],
@@ -383,14 +424,16 @@ if __name__ == '__main__':
         'dL': dL
     }
 
-    input_df, output_df, input_datasets = prep_data_for_model(
+    input_df, output_df, input_datasets, quantiles = prep_data_for_model(
         file_path=file_path,
         exp_name=exp_name,
         num_sample=num_sample,
         source_columns=input_columns,
         target_columns=output_columns,
         bin_feature=bin_feature,
-        sample_frac=sample_frac
+        sample_frac=sample_frac,
+        quantile_lower_bound=lower_limit,
+        quantile_upper_bound=upper_limit,
     )
     print(input_df.head())
     print(output_df.head())
@@ -398,3 +441,6 @@ if __name__ == '__main__':
     # Save the input and output data
     input_df.to_csv(f"{save_path}/{save_name}_input.csv", index=False)
     output_df.to_csv(f"{save_path}/{save_name}_output.csv", index=False)
+
+    # Save quantiles
+    pd.DataFrame(quantiles).to_csv(f"{save_path}/{save_name}_quantiles.csv", index=False)
