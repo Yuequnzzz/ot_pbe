66af23144ae17214bd1e37b957e27df8d15f57c2
Yuequnzzz <yueqzhang@student.ethz.ch>
2024-07-13 16:47:31 +0200

    added error_metrics

--- src/model_module/trainer.py
+++ src/model_module/trainer.py
@@ -13,7 +13,7 @@ import time
 # from src.model_module.data_processing_for_training import train_test_NN
 
 
-def convert_back_pdf(quantiles_array: np.ndarray):
+def convert_back_pdf(quantiles_array: np.ndarray) -> np.ndarray:
     """Convert quantiles back to pdf
 
     Args:
@@ -34,7 +34,7 @@ def calculate_wasserstein_distance(
         target_true_bins: np.ndarray,
         target_predicted_bins: np.ndarray,
         pdf_array: np.ndarray
-):
+) -> np.ndarray:
     """Calculate the Wasserstein distance between two pdfs
 
     Args:
@@ -51,6 +51,79 @@ def calculate_wasserstein_distance(
     wasserstein_distance_matrix = ot.emd2(pdf_array, pdf_array, cost_matrix)
 
     return wasserstein_distance_matrix
+
+
+def get_column_name_id(data: pd.DataFrame):
+    """Get the dictionary with the column name and the corresponding column ID
+
+    Args:
+        data (pd.DataFrame): Dataframe with the cleaned data
+
+    Returns:
+        dict: Dictionary with the column name and the corresponding column ID
+    """
+    dict_name_id = {column_name: column_id for column_id, column_name in enumerate(data.columns)}
+
+    return dict_name_id
+
+
+def calculate_error_metrics(
+        y_actual: np.ndarray,
+        y_predicted: np.ndarray,
+        pdf_array: np.ndarray,
+        dict_name_id: dict,
+):
+    """Calculate the error metrics
+
+    Args:
+        y_actual (np.ndarray): output actual values, including bins and other features
+        y_predicted (np.ndarray): output predicted values, including bins and other features
+        pdf_array (np.ndarray): Array with probability density values
+        dict_name_id (dict): Dictionary with the column name and the corresponding column ID
+
+    Returns:
+
+    """
+    # get the concentration column
+    # TODO: increase the flexibility here, add params in yaml
+    c_id = dict_name_id["c"]
+    c_rmse = mean_squared_error(y_actual[:, c_id], y_predicted[:, c_id], squared=False)
+
+    # get the num of crystals column
+    num_crystals_id = dict_name_id["mu_0"]
+    num_crystals_rmse = mean_squared_error(
+        y_actual[:, num_crystals_id],
+        y_predicted[:, num_crystals_id],
+        squared=False
+    )
+
+    # get the matrix of bins
+    bins_ids = [dict_name_id[k] if k.startswith("target_value") else None for k in dict_name_id.keys()]
+    bins_ids = [b for b in bins_ids if b is not None]
+    bins_actual = y_actual[:, bins_ids]
+    bins_predicted = y_predicted[:, bins_ids]
+
+    # calculate the wasserstein distance between the two pdfs
+    wasserstein_distance_matrix = calculate_wasserstein_distance(
+        target_true_bins=bins_actual,
+        target_predicted_bins=bins_predicted,
+        pdf_array=pdf_array
+    )
+
+    # TODO: add other error metrics like mu1, mu2,...
+
+    # get the total error
+    error_metrics = {
+        "c_rmse": c_rmse,
+        "num_crystals_rmse": num_crystals_rmse,
+        "wasserstein_distance_matrix": wasserstein_distance_matrix
+    }
+
+    return error_metrics
+
+
+
+
 
 
 # class Trainer:
